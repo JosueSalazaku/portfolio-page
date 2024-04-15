4ea0bed2a8fb45e2944b30eed8d43fdfcde0f5b4
Josué Salazaku <josuesalazaku@gmail.com>
2024-04-15 10:02:09 +0200

    Imporve header condition for Hamburger menu

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -5,39 +5,46 @@ import { RxHamburgerMenu } from "react-icons/rx";
 import { RxCross1 } from "react-icons/rx";
 import { useState } from "react";
 
-// eslint-disable-next-line react/prop-types
 function Navigation({ darkMode, toggleDarkMode }) {
-    const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-    const openMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const openMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const closeMenu = () => {
-        setIsOpen(false);
-    };
-
-
-
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
     <nav className="h-44 flex flex-row justify-between items-center px-20">
       <Link to="/Home">
         <h2 className="font-extrabold text-2xl">
-          JOSUE <br />SALAZAKU
+          JOSUE <br /> SALAZAKU
         </h2>
       </Link>
-      <div className="flex flex-row items-start gap-5">
+      <div className="flex flex-row items-center gap-5">
         <button
           onClick={toggleDarkMode}
-          className="px-8  py-2 text-white rounded-md"
+          className="px-8 py-2 text-white rounded-md"
         >
-          {darkMode ? <MdOutlineLightMode /> : <MdOutlineDarkMode className={darkMode ? "text-white" : "text-black"} />}
-              </button>
-              <section>
-                  <button className="h-20" onClick={openMenu}><RxHamburgerMenu style={{ fontSize: "24px" }} /></button>
-              </section>
-        <ul className="flex flex-col justify-start items-end gap-2 font-medium text-xl">
+          {darkMode ? (
+            <MdOutlineLightMode style={{ fontSize: "22px" }} />
+          ) : (
+            <MdOutlineDarkMode style={{ fontSize: "22px" }} className={darkMode ? "text-white" : "text-black"} />
+          )}
+        </button>
+        <section className="flex flex-row justify-start items-center">
+          <button className="h-20 justify-center items-center lg:hidden" onClick={openMenu}>
+            {isOpen ? (
+              <RxCross1 style={{ fontSize: "30px" }} />
+            ) : (
+              <RxHamburgerMenu style={{ fontSize: "30px" }} />
+            )}
+          </button>
+        </section>
+        {/* Conditionally render navigation links based on screen size */}
+        <ul className="flex-col justify-start items-end gap-2 font-medium text-xl hidden lg:flex">
           <li>
             <Link to="/About">About</Link>
           </li>
@@ -49,14 +56,18 @@ function Navigation({ darkMode, toggleDarkMode }) {
           </li>
         </ul>
       </div>
+      {/* Overlay menu for mobile */}
       {isOpen && (
-        <div className={darkMode ? "fixed inset-0 bg-neutral-800 bg-opacity-75" : "fixed bg-white inset-0 bg-opacity-75"} onClick={closeMenu}>
-          
-        </div>
+        <div
+          className={
+            darkMode
+              ? "fixed inset-0 bg-neutral-800 bg-opacity-75"
+              : "fixed bg-white inset-0 bg-opacity-75"
+          }
+          onClick={closeMenu}
+        ></div>
       )}
-      </nav>
-      
-      
+    </nav>
   );
 }
 
