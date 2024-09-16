b7326cec1826c422e144ab5fd2b4f0305543feea
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-16 21:48:38 +0200

    Improve padding on all sized

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,20 +1,29 @@
-
 function Home() {
   return (
-    <main className="mx-auto mb-14 w-full max-w-screen-sm flex-1 px-4 pb-8 pt-20 dark:text-white">
-      <section className="h-screen pt-20 flex flex-row gap-5">
-        <img className="rounded-full size-[50px]" src="public/josuesalazaku.png" alt="Profile Picture" />
-        <div>
-        <p className="font-sans">
-          JOSUE SALAZAKU
-        </p>
-        <p>
-          Visual Artist / Web Developer
-        </p>
+    <main className="mx-auto mb-14 w-full max-w-screen-sm flex-1 px-4 ">
+      <section className="h-screen pt-12 flex flex-col gap-10">
+        <div className="flex gap-4 items-center">
+          <img
+            className="rounded-full size-[60px]"
+            src="public/josuesalazaku.png"
+            alt="Profile Picture"
+          />
+          <div className="font-sans">
+            <p className="font-bold">Josué Salazaku</p>
+            <p className="font-light">Visual Artist / Web Developer</p>
           </div>
+        </div>
+        <div className="font-sans">
+          <p>About me</p>
+          <p className="font-light">Add text</p>
+        </div>
+        <div className="font-sans">
+          <p>Now</p>
+          <p className="font-light">Add text</p>
+        </div>
       </section>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
