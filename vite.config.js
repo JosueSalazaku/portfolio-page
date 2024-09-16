7806f6ee9eab30d50c3a4b2fd8dc1910b769353e
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-16 22:15:25 +0200

    Update page title in index.html and configure Vite for production build and routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Vercel will use the `dist` folder as the build output by default
+  },
+  base: './', // Ensures relative paths, which can be important for proper routing and asset loading in production
+});
