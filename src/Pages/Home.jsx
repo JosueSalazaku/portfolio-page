0cc76ff9425d248a4cb771babd66a4ecc34d1d13
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-17 01:44:08 +0200

    Refactor Home component to update font weight and about me section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
-function Home() {
+export default function Home() {
   return (
-    <main className="mx-auto font-roboto mb-14 w-full max-w-screen-sm flex-1 px-4 pb-8">
+    <main className="mx-auto font-roboto text-[17px] mb-14 w-full max-w-screen-sm flex-1 px-4 pb-8">
       <section className="h-screen pt-20 flex flex-col gap-5">
         <div className="flex gap-5 items-center">
           <img
@@ -8,16 +8,16 @@ function Home() {
             src="josuesalazaku.png"
             alt="Profile Picture"
           />
-          <div className="font-semibold text-[18px]">
-            <p>Josué Salazaku</p>
-            <p className="font-light">
+          <div className="">
+            <p className="font-meduim">Josué Salazaku</p>
+            <p className="text-zinc-500">
               Visual Artist / Web Developer
             </p>
           </div>
         </div>
         <div className="pt-8 space-y-3">
-          <p className="font-semibold">About me</p>
-          <p className="font-normal">
+          <p className="font-meduim">About me</p>
+          <p className="text-zinc-500">
             Creating vibrant paintings that explore emotions, stories, and
             abstract concepts into vivid, expressive artworks. Balancing my
             artistic practice with professional web development, I focus on
@@ -30,5 +30,3 @@ function Home() {
     </main>
   );
 }
-
-export default Home;
