0b6d086766de332d2655774af1f91c7d1c7b38c1
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-16 22:21:01 +0200

    Refactor routing in App component to set Home as the default page

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <main>
         <Routes>
-          <Route path="/Home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/Work" element={<Work />} />
