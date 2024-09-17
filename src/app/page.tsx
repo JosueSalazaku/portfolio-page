eff8bbd23907893c962ce4300e982f270a046542
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-17 21:35:42 +0200

    Refactor project to use Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+import Image from "next/image";
+
+
 export default function Home() {
   return (
     <main className="mx-auto font-roboto text-[17px] mb-14 w-full max-w-screen-sm flex-1 px-4 pb-8">
       <section className="h-screen pt-20 flex flex-col gap-5">
         <div className="flex gap-5 items-center">
-          <img
+          <Image
             className="rounded-full size-[65px]"
-            src="josuesalazaku.png"
+            src="/josuesalazaku.png"
             alt="Profile Picture"
+            width={65}
+            height={65}
           />
           <div className="">
             <p className="font-meduim">Josué Salazaku</p>
@@ -17,13 +22,11 @@ export default function Home() {
         </div>
         <div className="pt-8 space-y-3">
           <p className="font-meduim">About me</p>
-          <p className="text-zinc-500">
-            Creating vibrant paintings that explore emotions, stories, and
-            abstract concepts into vivid, expressive artworks. Balancing my
-            artistic practice with professional web development, I focus on
-            crafting visually compelling and functional websites. Passionate
-            about blending creativity with functionality, I aim to deliver
-            unique experiences both on canvas and online.
+          <p className="text-zinc-300">
+            Front-end developer based in Antwerp, Belgium, focused building visually engaging and
+            functional websites I blend creativity with practicality to create user-friendly
+            experiences that work seamlessly. I aim to deliver websites that are both
+            aesthetically appealing and intuitive for users.
           </p>
         </div>
       </section>
