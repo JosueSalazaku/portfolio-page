8f1fbdf4ef11aec97ed2b88a9b87194eb265e1eb
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-17 22:22:04 +0200

    maka the nav component stick

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function Nav() {
   return (
-      <nav className='mx-auto h-[50px] flex flex-col justify-center items-center rounded border-white'>
+      <nav className='mx-auto h-[50px] flex flex-col justify-center items-center rounded border-white  fixed bottom-20 left-0 right-0 z-50'>
           <div>
             <ul className='w-[250px] h-[50px] flex flex-row items-center px-10 justify-between rounded-md bg-slate-900'>
               <Link href="/"><li>H</li></Link>
