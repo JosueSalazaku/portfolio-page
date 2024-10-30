d4b898a3802294caf43a74efb6c01b3e5b7c6710
Josué Salazaku <josuesalazaku@gmail.com>
2024-10-30 11:07:47 +0100

    Refactor import path for useTheme in ModeToggle component to use relative path

--- src/app/components/ModeToggle.tsx
+++ src/app/components/ModeToggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 import * as React from "react";
 import { Moon, Sun } from "lucide-react";
-import { useTheme } from "@/app/components/ThemeProvider";
+import { useTheme } from "./ThemeProvider";
 
 export function ModeToggle() {
   const { theme, toggleTheme } = useTheme();
