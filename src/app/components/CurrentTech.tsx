ffbcb1693cfd6eef70f0fb4eb43641433acec9a5
Josué Salazaku <josuesalazaku@gmail.com>
2024-10-30 19:50:58 +0100

    Refactor: clean up formatting and improve text layout in CurrentTech component

--- src/app/components/CurrentTech.tsx
+++ src/app/components/CurrentTech.tsx
@@ -11,37 +11,27 @@ function CurrentTech() {
     { color: "#3A130D", alt: "Git", image: "/git.svg" },
     { color: "#12283A", alt: "Postgres", image: "/PostgreSQL.svg" },
     { color: "#1A3618", alt: "Express.js", image: "/Express.svg" },
-    { color: "#394C14", alt: "Drizzle ORM", image: "/drizzle.svg" },    
+    { color: "#394C14", alt: "Drizzle ORM", image: "/drizzle.svg" },
   ];
 
   return (
     <section className="flex flex-col pt-12 pb-12 ">
-      <h1 className="font-bold dark:text-white">
-        Current Technologies
-      </h1>
+      <h1 className="font-bold dark:text-white">Current Technologies</h1>
       <p className="mt-4 text-gray-lightmode dark:text-gray-darkmode">
-        Here are some of the main tools and tech I use to
-        build awesome stuff. From frameworks to libraries,
-        these are the go-to technologies that help bring my
-        ideas to life.
+        Here are some of the main tools and tech I use to build awesome stuff. From frameworks to libraries, these are the go-to technologies that help bring my ideas to life.
       </p>
-      <div id="logo" className="flex flex-row gap-[11px] pt-8  md:gap-9 lg:gap-[38px]">
-
+      <div id="logo" className="flex flex-row gap-[11px] pt-8 md:gap-9 lg:gap-[38px]">
         {techs.map((tech, index) => (
           <div
             key={index}
+            title={tech.alt} 
             className="flex flex-col items-center"
           >
             <div
               className="flex items-center justify-center w-10 h-10 rounded"
               style={{ backgroundColor: tech.color }}
             >
-              <Image
-                src={tech.image}
-                alt={tech.alt}
-                width={25}
-                height={20}
-              />
+              <Image src={tech.image} alt={tech.alt} width={20} height={20} />
             </div>
           </div>
         ))}
