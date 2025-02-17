cd82097a7c6f5bd294f587ed6d8787a140f26444
Josué Salazaku <josuesalazaku@gmail.com>
2025-02-18 00:46:19 +0100

    refactor: adjust project card styles and improve layout in portfolio page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function page() {
   return (
     <div className="flex flex-col gap-5 pt-20 pb-12">
-      <h1 className="pb-6 text-2xl font-bold dark:text-white">My Portfolio</h1>
-      <h1 className="py-4 text-gray-lightmode">Latest ongoing projects</h1>
+      <h1 className="pb-4 text-2xl font-bold dark:text-white">My Portfolio</h1>
+      <h1 className=" text-gray-lightmode">Latest ongoing projects</h1>
       {/* Portfolio Grid */}
       <div className="grid w-full max-w-screen-lg grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-2">
         {/* Project Card */}
@@ -14,22 +14,24 @@ export default function page() {
             <Image
               src="/piles.png"
               alt="Journo Project"
-              width={840}
-              height={40}
+              width={940}
+              height={50}
               className="object-cover w-full h-full"
             />
-            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black/80 via-black/40 to-transparent">
-              <h2 className="text-lg font-bold text-white">Unread Piles</h2>
-              <div className="flex flex-wrap gap-2 mt-2">
-                <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
-                  Next.js
-                </span>
-                <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
-                  TailwindCSS
-                </span>
-                <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
-                  Postgress
-                </span>
+            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-black/90 to-gray-900 h-3/4">
+              <div className="absolute bottom-0 left-0 right-0 p-4">
+                <h2 className="text-lg font-bold text-white">Unread Piles</h2>
+                <div className="flex flex-wrap gap-2 mt-2">
+                  <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
+                    Next.js
+                  </span>
+                  <span className="px-2 py-1 text-xs rounded text-cyan-500 bg-gray-dark">
+                    TailwindCSS
+                  </span>
+                  <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
+                    Postgress
+                  </span>
+                </div>
               </div>
             </div>
           </div>
@@ -45,18 +47,20 @@ export default function page() {
               height={40}
               className="object-cover w-full h-full"
             />
-            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black/80 via-black/40 to-transparent">
-              <h2 className="text-lg font-bold text-white">Keanino</h2>
-              <div className="flex flex-wrap gap-2 mt-2">
-                <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
-                  Next.js
-                </span>
-                <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
-                  TailwindCSS
-                </span>
-                <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
-                  Drizzle
-                </span>
+            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-black/90 to-gray-900 h-3/4">
+              <div className="absolute bottom-0 left-0 right-0 p-4">
+                <h2 className="text-lg font-bold text-white">Keanino</h2>
+                <div className="flex flex-wrap gap-2 mt-2">
+                  <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
+                    Next.js
+                  </span>
+                  <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
+                    TailwindCSS
+                  </span>
+                  <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
+                    Drizzle
+                  </span>
+                </div>
               </div>
             </div>
           </div>
@@ -74,15 +78,17 @@ export default function page() {
             height={140}
             className="object-cover w-full h-full"
           />
-          <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black/80 via-black/40 to-transparent">
-            <h2 className="text-lg font-bold text-white">LinkUp</h2>
-            <div className="flex flex-wrap gap-2 mt-2">
-              <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
-                React
-              </span>
-              <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
-                TypeScript
-              </span>
+          <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black via-black/90 to-gray-900 h-3/4">
+            <div className="absolute bottom-0 left-0 right-0 p-4">
+              <h2 className="text-lg font-bold text-white">LinkUp</h2>
+              <div className="flex flex-wrap gap-2 mt-2">
+                <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
+                  React
+                </span>
+                <span className="px-2 py-1 text-xs text-white rounded bg-gray-dark">
+                  TypeScript
+                </span>
+              </div>
             </div>
           </div>
         </Link>
